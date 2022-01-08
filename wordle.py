362216ffdb3136084eeca18baf2b5df13812906d
Mark Tilton <mark.tilton.a@gmail.com>
2022-01-08 13:43:37 -0800

    Format script

--- wordle.py
+++ wordle.py
@@ -9,18 +9,25 @@ with open("words.txt", mode='r') as f:
     words = f.readlines()
 words = [word.strip() for word in words]
 
+
 def get_letter_scores(words):
     all_letters = (letter for word in words for letter in word)
     return Counter(all_letters)
 
+
 def get_word_score(word, letter_score):
     return sum(letter_score[letter] for letter in set(word))
 
+
 global_letter_score = get_letter_scores(words)
-global_word_score = {word: get_word_score(word, global_letter_score) for word in words}
-sorted_words = sorted(global_word_score, key=global_word_score.get, reverse=True)
+global_word_score = {word: get_word_score(
+    word, global_letter_score) for word in words}
+sorted_words = sorted(
+    global_word_score, key=global_word_score.get, reverse=True)
 
 # Calculate the number of turns it takes to get to word from start
+
+
 def play_game(goal_word, input_source, display=lambda _="": ()):
     turn = 0
     found_letters = [None] * 5
@@ -36,7 +43,8 @@ def play_game(goal_word, input_source, display=lambda _="": ()):
         display(f"Invalid letters: {invalid_letters}")
 
         # Make a guess
-        guess_word = input_source(found_letters, loose_letters, invalid_letters, guesses)
+        guess_word = input_source(
+            found_letters, loose_letters, invalid_letters, guesses)
         guesses.append(guess_word)
         display(f"Guessed {guess_word}")
         if guess_word == goal_word:
@@ -50,7 +58,7 @@ def play_game(goal_word, input_source, display=lambda _="": ()):
                 found_letters[i] = guess_letter
                 if guess_letter in loose_letters:
                     loose_letters.remove(guess_letter)
-        
+
         # Create a pool of remaining letters to match against
         goal_letters = [l for l in goal_word]
         guess_letters = [l for l in guess_word]
@@ -59,7 +67,7 @@ def play_game(goal_word, input_source, display=lambda _="": ()):
                 goal_letters.remove(found_letter)
                 if guess_word[i] == found_letter:
                     guess_letters.remove(found_letter)
-        
+
         # Remove the existing letters from the loose pool to avoid repeats
         for l in guess_letters:
             if l in loose_letters:
@@ -71,7 +79,7 @@ def play_game(goal_word, input_source, display=lambda _="": ()):
             if l in goal_letters:
                 loose_letters.append(l)
                 goal_letters.remove(l)
-        
+
         # Add uniquely invalid letters to the invalid pool
         for l in guess_word:
             if l not in goal_word:
@@ -84,7 +92,7 @@ def check_word(word, found_letters, loose_letters, invalid_letters):
     for l in word:
         if l in invalid_letters:
             return False
-    
+
     # Are all the found letters in the correct spot?
     for wl, fl in zip(word, found_letters):
         if fl != None and fl != wl:
@@ -96,7 +104,7 @@ def check_word(word, found_letters, loose_letters, invalid_letters):
         if found_letter != None:
             if word[i] == found_letter:
                 guess_letters.remove(found_letter)
-    
+
     # Are ALL loose letters contained within the remaining letters?
     for l in loose_letters:
         if l in guess_letters:
@@ -104,12 +112,14 @@ def check_word(word, found_letters, loose_letters, invalid_letters):
             guess_letters.remove(l)
         else:
             return False
-    
+
     return True
 
 # Pick the best word based on global letter scores.
 # Picks the highest scoring word that is still possible and hasn't already been guessed
 # Only provides valid guesses
+
+
 def pick_best_word_v1(found_letters, loose_letters, invalid_letters, guesses):
     for word in sorted_words:
         if word in guesses:
@@ -121,30 +131,38 @@ def pick_best_word_v1(found_letters, loose_letters, invalid_letters, guesses):
 # 1. Get all the remaining valid words
 # 2. Recalculate letter scores for the remaining letters
 # Only provides valid guesses
+
+
 def pick_best_word_v2(found_letters, loose_letters, invalid_letters, guesses):
-    valid_words = [word for word in words 
-        if check_word(word, found_letters, loose_letters, invalid_letters) 
-        and word not in guesses]
+    valid_words = [word for word in words
+                   if check_word(word, found_letters, loose_letters, invalid_letters)
+                   and word not in guesses]
     letter_score = get_letter_scores(valid_words)
-    return max((word for word in valid_words if word not in guesses), 
-        key=lambda word: get_word_score(word, letter_score))
+    return max((word for word in valid_words if word not in guesses),
+               key=lambda word: get_word_score(word, letter_score))
 
 # Picks a guess based on the following heuristic
 # 1. Use found spaces to explore remaining letters. Remaining letters should (preferrably) be explored
 #    in the order of their prevalence in the remaining words.
 # 2. Explore new positions for loose letters. This heuristic should stop us from getting stuck in loops
 #    when an anagram is found.
+
+
 def pick_best_word_v3(found_letters, loose_letters, invalid_letters, guesses):
     pass
 
+
 def evaluate_strategy(strategy):
-    word_difficulty = Parallel(n_jobs=16)(delayed(play_game)(goal_word=word, input_source=strategy) for word in tqdm(words))
+    word_difficulty = Parallel(n_jobs=16)(delayed(play_game)(
+        goal_word=word, input_source=strategy) for word in tqdm(words))
     difficulty_spread = Counter(word_difficulty)
     average_turns = sum(word_difficulty) / len(words)
-    num_solved = len([turn_count for turn_count in word_difficulty if turn_count <= 6])
+    num_solved = len(
+        [turn_count for turn_count in word_difficulty if turn_count <= 6])
     print(f"{difficulty_spread=}")
     print(f"{average_turns=}")
     print(f"{num_solved=}")
+
 
 print("V1")
 evaluate_strategy(pick_best_word_v1)
